46d471210e414f956b35b2e134b874faf0bfbfc9
Nakwon Yoon <yoonnw1000@gmail.com>
2020-08-24 13:34:10 -0500

    adding some comments

--- client/src/components/NavTabs/index.js
+++ client/src/components/NavTabs/index.js
@@ -21,17 +21,18 @@ const NavTab = (props) => {
             <NavbarBrand href="/">Movie Librariran</NavbarBrand>
               <Nav className="mr-auto" navbar>
                 <NavItem>
+      {/* since we have the path setting on App.js we don't need href but I put them on just in case for now. */}
                   <NavLink href="/Home">Home</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink href="/library">library</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink href="/wishlist/">Wishlist</NavLink>
+                  <NavLink href="/wishlist">Wishlist</NavLink>
                 </NavItem>
               </Nav>
               <NavbarText> Hi, {props.username}</NavbarText> 
-                  <NavLink className="float-right" href="/">Log-out</NavLink>
+                  <NavLink className="float-right" href="/">Logo out</NavLink>
 
           </Navbar>
         </div>
