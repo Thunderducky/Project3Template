6159c0f7bca286a5a04305f16277c2ed9ff5ae28
Nakwon Yoon <yoonnw1000@gmail.com>
2020-08-25 11:33:29 -0500

    updating some style

--- client/src/components/Grid/Container.js
+++ client/src/components/Grid/Container.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+
 
 // This Container component allows us to use a bootstrap container without worrying about class names
 export function Container({ fluid, children }) {
