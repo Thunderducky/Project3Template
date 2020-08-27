c07921f05e3fe9b0f9361483e5c7a2bb0cb669af
Alex Crouch <acrouch06@gmail.com>
2020-08-26 21:17:55 -0500

    added badges next to movies on all tab

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -13,6 +13,7 @@ import {
   Jumbotron,
   Container,
   Media,
+  Badge
 } from "reactstrap";
 import classnames from "classnames";
 import SqlAPI from "../utils/SQL-API";
@@ -77,6 +78,19 @@ const LibraryTab = () => {
     SqlAPI.deleteMovie(id)
     const newList = movieList.filter(item => item.id !== id)
     setMovieList(newList);
+  }
+
+  const renderBadges = function(movieObject) {
+    switch (movieObject.format) {
+      case "DVD":
+        return <Badge color="success" pill>DVD</Badge>
+      case "BluRay":
+        return <Badge color="primary" pill>BluRay</Badge>
+      case "VOD":
+        return <Badge color="warning" pill>VOD</Badge>
+      default:
+        break;
+    }
   }
 
   return (
@@ -153,7 +167,7 @@ const LibraryTab = () => {
                               />
                             </Media>
                             <Media body className="movieBody">
-                              <Media heading><strong>{movie.title}</strong></Media>
+                              <Media heading><strong>{movie.title} {'\('+movie.year+'\)'} {renderBadges(movie)}</strong></Media>
                               {movie.synopsis}
                               <br />
                               <Button className="deleteBtn" outline color="danger" size="sm" id={movie.id} onClick={handleDelete}>Remove from Shelf</Button>
@@ -187,10 +201,10 @@ const LibraryTab = () => {
                               />
                             </Media>
                             <Media body className="movieBody">
-                              <Media heading><strong>{movie.title}</strong></Media>
+                              <Media heading><strong>{movie.title} {'\('+movie.year+'\)'}</strong></Media>
                               {movie.synopsis}
                               <br />
-                              <Button className="deleteBtn" outline color="danger" size="sm" value={movie.id} >Remove from Shelf</Button>
+                              <Button className="deleteBtn" outline color="danger" size="sm" id={movie.id} onClick={handleDelete} >Remove from Shelf</Button>
                             </Media>
                           </Media>
                         </ListGroupItem>
@@ -221,10 +235,10 @@ const LibraryTab = () => {
                               />
                             </Media>
                             <Media body className="movieBody">
-                              <Media heading><strong>{movie.title}</strong></Media>
+                              <Media heading><strong>{movie.title} {'\('+movie.year+'\)'}</strong></Media>
                               {movie.synopsis}
                               <br />
-                              <Button className="deleteBtn" outline color="danger" size="sm" value={movie.id} >Remove from Shelf</Button>
+                              <Button className="deleteBtn" outline color="danger" size="sm" id={movie.id} onClick={handleDelete} >Remove from Shelf</Button>
                             </Media>
                           </Media>
                         </ListGroupItem>
@@ -255,10 +269,10 @@ const LibraryTab = () => {
                               />
                             </Media>
                             <Media body className="movieBody">
-                              <Media heading><strong>{movie.title}</strong></Media>
+                              <Media heading><strong>{movie.title} {'\('+movie.year+'\)'}</strong></Media>
                               {movie.synopsis}
                               <br />
-                              <Button className="deleteBtn" outline color="danger" size="sm" value={movie.id} >Remove from Shelf</Button>
+                              <Button className="deleteBtn" outline color="danger" size="sm" id={movie.id} onClick={handleDelete} >Remove from Shelf</Button>
                             </Media>
                           </Media>
                         </ListGroupItem>
