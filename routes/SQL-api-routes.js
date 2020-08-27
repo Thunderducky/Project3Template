5405f9160e579460071380cd274fc4b4e600c967
Alex Crouch <acrouch06@gmail.com>
2020-08-26 23:06:07 -0500

    added imdbID as a field

--- routes/SQL-api-routes.js
+++ routes/SQL-api-routes.js
@@ -4,74 +4,79 @@ module.exports = function (app) {
   //Find all movies associated with user AND join their userID from users model
   app.get("/api/movies", async function (req, res) {
     const uid = req.user.id;
-      db.Movie.findAll(
-        { where: { UserId: uid, wishlist: false} },
-        { include: db.User }
-      ).then(function(dbMovie) {
+    db.Movie.findAll(
+      { where: { UserId: uid, wishlist: false } },
+      { include: db.User }
+    )
+      .then(function (dbMovie) {
         res.json(dbMovie);
-    }).catch(function(err) {
+      })
+      .catch(function (err) {
         res.status(500).json(err);
-        console.log(req.body)
+        console.log(req.body);
       });
-}),
+  }),
     app.get("/api/wishlist", async function (req, res) {
       const uid = req.user.id;
       db.Movie.findAll(
-          { where: { UserId: uid, wishlist: true } },
-          { include: db.User }
-        ).then(function(dbMovie) {
-            res.json(dbMovie);
-        }).catch(function(err) {
-            res.status(500).json(err);
-            console.log(req.body)
-          });
+        { where: { UserId: uid, wishlist: true } },
+        { include: db.User }
+      )
+        .then(function (dbMovie) {
+          res.json(dbMovie);
+        })
+        .catch(function (err) {
+          res.status(500).json(err);
+          console.log(req.body);
+        });
     }),
     // grabs all movies of specified format and not in wishlist
     app.get("/api/movies/:format", async function (req, res) {
       const uid = req.user.id;
       const format = req.params.format;
       db.Movie.findAll(
-          { where: { UserId: uid, format: format, wishlist: false } },
-          { include: db.User }
-        ).then(function(dbMovie) {
-            res.json(dbMovie);
-        }).catch(function(err) {
-            res.status(500).json(err);
-            console.log(req.body)
-          });
+        { where: { UserId: uid, format: format, wishlist: false } },
+        { include: db.User }
+      )
+        .then(function (dbMovie) {
+          res.json(dbMovie);
+        })
+        .catch(function (err) {
+          res.status(500).json(err);
+          console.log(req.body);
+        });
     }),
     // grabs movie with specific id
     app.get("/api/movies/:id", function (req, res) {
       const uid = req.user.id;
       const id = req.params.id;
-      db.Movie.findOne(
-          { where: { UserId: uid, id: id } },
-          { include: db.User }
-        ).then(function(dbMovie) {
-            res.json(dbMovie);
-        }).catch(function(err) {
-            res.status(500).json(err);
-            console.log(req.body)
-          });
+      db.Movie.findOne({ where: { UserId: uid, id: id } }, { include: db.User })
+        .then(function (dbMovie) {
+          res.json(dbMovie);
+        })
+        .catch(function (err) {
+          res.status(500).json(err);
+          console.log(req.body);
+        });
     }),
     app.post("/api/movies", async function (req, res) {
-        try{
-            const uid = req.user.id;
-            req.body.UserId = uid;
-            let dbMovie = await db.Movie.create(req.body);
-            res.json(dbMovie);
-        }
-        catch(err) {
-            res.status(500).json(err);
-            console.log(req.body)
-      };
+      const uid = req.user.id;
+      req.body.UserId = uid;
+      db.Movie.create(req.body)
+        .then(function (dbMovie) {
+          res.json(dbMovie);
+        })
+        .catch(function (err) {
+          res.status(500).json(err);
+          console.log(req.body);
+        });
     }),
     app.delete("/api/movies/:id", function (req, res) {
       db.Movie.destroy({
-          where: { id: req.params.id },
-        }).catch(function(err) {
-            res.status(500).json(err);
-            console.log(req.body)
-          });
+        where: { id: req.params.id },
+      }).catch(function (err) {
+        res.status(500).json(err);
+        console.log(req.body);
+      });
     });
 };
